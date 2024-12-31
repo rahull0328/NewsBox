a155004160503545f6a3e5604421017ace920678
rahull0328 <rahull0328@users.noreply.github.com>
2024-12-31 18:18:32 +0530

    Added UI To display the news

--- home/views.py
+++ home/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from .utils import *
-from .secrets import API_KEY
 
 # Create your views here.
 def home(request):
     url = 'https://newsapi.org/v2/everything?q=tesla&from=2024-11-30&sortBy=publishedAt&apiKey=a47e7903f2984016880269ddbbf4ed70'
     get_news_from_api(url)
-    return render(request, 'home.html')
+    return render(request, 'home.html', context = {'articles' : Articles.objects.all()})
